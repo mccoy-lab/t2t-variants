3b4cf92a42b11cb4bea74082d530da97b02a7c41
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2021-06-02 15:57:42 -0400

    Update rare_haps_wrapper_ref.sh

--- rare_haps_wrapper_ref.sh
+++ rare_haps_wrapper_ref.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=rare_haps_chm13
+#SBATCH --job-name=rare_haps_NA21143
 #SBATCH -N 1
 #SBATCH --cpus-per-task 48
 #SBATCH --time=36:00:00
@@ -15,24 +15,24 @@
 cd /scratch4/mschatz1/rmccoy22/rmccoy22/rare_haplotypes/
 
 # replace with GRCh38 if desired
-SAMPLE='CHM13'
+SAMPLE='NA21143'
 
 mkdir -p /scratch4/mschatz1/rmccoy22/rmccoy22/rare_haplotypes/${SAMPLE}
 mkdir -p /scratch4/mschatz1/rmccoy22/rmccoy22/rare_haplotypes/output
 
 cd /scratch4/mschatz1/rmccoy22/rmccoy22/rare_haplotypes/${SAMPLE}
 
-cat ../1KGP_samples.txt | awk '{print $1"\t"$1}' > ${SAMPLE}.keep
+cat ../1KGP_samples.txt | grep -v -w ${SAMPLE} | awk '{print $1"\t"$1}' > ${SAMPLE}.keep
 
-~/scratch4-mschatz1/rmccoy22/code/bcftools-1.11/bcftools norm \
--o ${SAMPLE}_chr${SLURM_ARRAY_TASK_ID}.vcf.gz \
--O z \
---threads 48 \
--m +snps \
-/scratch4/mschatz1/rmccoy22/1kg-GRCh38-NYGC-highcoverage/CCDG_14151_B01_GRM_WGS_2020-08-05_chr${SLURM_ARRAY_TASK_ID}.filtered.shapeit2-duohmm-phased.vcf.gz
+#~/scratch4-mschatz1/rmccoy22/code/bcftools-1.11/bcftools norm \
+#-o ${SAMPLE}_chr${SLURM_ARRAY_TASK_ID}.vcf.gz \
+#-O z \
+#--threads 48 \
+#-m +snps \
+#/scratch4/mschatz1/rmccoy22/1kg-GRCh38-NYGC-highcoverage/CCDG_14151_B01_GRM_WGS_2020-08-05_chr${SLURM_ARRAY_TASK_ID}.filtered.shapeit2-duohmm-phased.vcf.gz
 
 ~/scratch4-mschatz1/rmccoy22/code/plink \
---vcf ${SAMPLE}_chr${SLURM_ARRAY_TASK_ID}.vcf.gz \
+--vcf /scratch4/mschatz1/rmccoy22/1kg-GRCh38-NYGC-highcoverage/CCDG_14151_B01_GRM_WGS_2020-08-05_chr${SLURM_ARRAY_TASK_ID}.filtered.shapeit2-duohmm-phased.vcf.gz \
 --make-bed \
 --keep-allele-order \
 --snps-only \
