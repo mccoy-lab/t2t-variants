6010112a50406fc2872e7f60aea93ec81ab4d4d8
dtaylo95 <dtaylo95@jhu.edu>
2021-07-14 17:36:16 -0400

    Renamed liftover script files

--- liftover_vcfs/subset_to_non-recovered.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-recovered_file=$1
-full_rejected_file=$2
-not_recovered_output=$3
-
-subset_script=subset_to_non-recovered.py
-
-python python ${subset_script} ${recovered_file} ${full_rejected_file} ${not_recovered_output}
