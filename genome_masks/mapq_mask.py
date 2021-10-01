2b3f00d54dcf46ae8626ac3524e06fd5734f65be
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2021-10-01 15:20:50 -0400

    update merged file path

--- genome_masks/mapq_mask.py
+++ genome_masks/mapq_mask.py
@@ -5,8 +5,7 @@ import statistics
 chrom = sys.argv[1]
 ref = "/scratch4/mschatz1/CHM13/drafts/t2t-chm13.20200921.withGRCh38chrY.chrEBV.chrYKI270740v1r.fasta"
 
-# to do: swap in the merged file below
-aln = pysam.AlignmentFile("/scratch4/mschatz1/mschatz/T2T/2021.09.29.mask/HG03122.cram", "rb", reference_filename = ref)
+aln = pysam.AlignmentFile("/scratch4/mschatz1/mschatz/T2T/2021.09.29.mask/" + chrom + "_merged.cram", "rb", reference_filename = ref)
 
 for rec in aln.pileup(chrom, truncate = True):
   mapq = rec.get_mapping_qualities()
