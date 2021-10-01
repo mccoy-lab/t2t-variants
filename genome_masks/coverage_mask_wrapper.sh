f021000c614e069e10d529b58dd269bf755bb8dd
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2021-10-01 09:57:04 -0400

    Create coverage_mask_wrapper.sh

---
+++ genome_masks/coverage_mask_wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=cov_mask
+#SBATCH -N 1
+#SBATCH --cpus-per-task 48
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=end
+#SBATCH --mail-user=rmccoy22@jhu.edu
+#SBATCH --array=1-22%22
+
+cd /scratch4/mschatz1/rmccoy22/rmccoy22/mask
+
+chrom="chr${SLURM_ARRAY_TASK_ID}"
+
+python coverage_mask.py ${chrom} > coverage_mask_${chrom}.txt
