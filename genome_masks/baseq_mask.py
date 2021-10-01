a30a85e07759840926b1346741ccf100147e1938
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2021-10-01 15:21:08 -0400

    update merged file path

--- genome_masks/baseq_mask.py
+++ genome_masks/baseq_mask.py
@@ -6,7 +6,7 @@ chrom = sys.argv[1]
 ref = "/scratch4/mschatz1/CHM13/drafts/t2t-chm13.20200921.withGRCh38chrY.chrEBV.chrYKI270740v1r.fasta"
 
 # to do: swap in the merged file below
-aln = pysam.AlignmentFile("/scratch4/mschatz1/mschatz/T2T/2021.09.29.mask/HG03122.cram", "rb", reference_filename = ref)
+aln = pysam.AlignmentFile("/scratch4/mschatz1/mschatz/T2T/2021.09.29.mask/" + chrom + "_merged.cram", "rb", reference_filename = ref)
 
 for rec in aln.pileup(chrom, truncate = True):
   baseq = rec.get_query_qualities()
