6f374ec4580c357de28403a65ee0e36b1df3b267
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2021-10-01 22:13:08 -0400

    add sex chromosomes

--- genome_masks/coverage_mask_wrapper.sh
+++ genome_masks/coverage_mask_wrapper.sh
@@ -6,10 +6,20 @@
 #SBATCH --time=12:00:00
 #SBATCH --mail-type=end
 #SBATCH --mail-user=rmccoy22@jhu.edu
-#SBATCH --array=1-22%22
+#SBATCH --array=1-24%24
 
 cd /scratch4/mschatz1/rmccoy22/rmccoy22/mask
 
-chrom="chr${SLURM_ARRAY_TASK_ID}"
+if [[ ${chrom} -eq 23 ]]
+then
+  chrom="chrX"
+elif [[ ${chrom} -eq 24 ]]
+then
+  chrom="chrY"
+else
+  chrom="chr${SLURM_ARRAY_TASK_ID}"
+fi
+
+
 
 python coverage_mask.py ${chrom} > coverage_mask_${chrom}.txt
