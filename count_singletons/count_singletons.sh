4b2153d3e47230e2f527d5b675db889a0d874dc2
Rajiv McCoy <rajiv.mccoy@jhu.edu>
2021-10-15 13:18:17 -0400

    sample list

--- count_singletons/count_singletons.sh
+++ count_singletons/count_singletons.sh
@@ -25,8 +25,9 @@ vcftools \
 PATH=${PATH}:"~/scratch4-mschatz1/rmccoy22/code/bcftools-1.11/"
 
 bcftools query \
-  --samples unrelated_samples.keep \
+  --samples-file unrelated_samples.keep \
   -i'AC==AN' \
   -f'%CHROM %POS %AC %AN\n' \
   /scratch4/mschatz1/rmccoy22/1kg-CHM13-recalibrated-PASS/1kgp.chr${SLURM_ARRAY_TASK_ID}.recalibrated.snp_indel.pass.vcf.gz \
   > chr${SLURM_ARRAY_TASK_ID}_CHM13_singletons.txt 
+  
