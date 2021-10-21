fc783b6125df9b0de48f325e2ad38d6f01b4b5b9
Dylan Taylor <54147660+dtaylo95@users.noreply.github.com>
2021-10-21 18:38:16 -0400

    Update resolve_refalt_switches_in_multialt.sh

--- liftover_vcfs/resolve_refalt_switches_in_multialt.sh
+++ liftover_vcfs/resolve_refalt_switches_in_multialt.sh
@@ -7,11 +7,13 @@ CHAIN="/scratch4/mschatz1/rmccoy22/CHM13/hg38.t2t-chm13-v1.0.over.chain"
 REF="/scratch4/mschatz1/rmccoy22/CHM13/chm13.draft_v1.0.fasta"
 
 foi=$1 # This should be the rejected vcf file from the initial liftover
+recovered_out_file=$2 # should end with .gz
+
 parsed_multiallelic=$1.parsed.vcf.gz
 tmp_out_lifted=$1.parsed.lifted.vcf.gz
 tmp_out_rejected=$1.parsed.rejected.vcf.gz
 recovered_info=$1.parsed.recovered.info
-recovered_out_file=$2
+
 
 zcat ${foi} | python ${parser} | gzip -c > ${parsed_multiallelic}
 ${GATK} LiftoverVcf -I ${parsed_multiallelic} -O ${tmp_out_lifted} --CHAIN ${CHAIN} -R ${REF} --REJECT ${tmp_out_rejected} --RECOVER_SWAPPED_REF_ALT --WRITE_ORIGINAL_POSITION --WRITE_ORIGINAL_ALLELES
@@ -21,10 +23,13 @@ mv ${tmp_out_lifted}.corrected ${tmp_out_lifted}
 
 zcat ${tmp_out_lifted} | grep -a ^[^#] | cut -f 1-5,8 > ${recovered_info}
 
-python ${resolver} ${recovered_info} ${foi} ${tmp_out_lifted} ${recovered_out_file}
+python ${resolver} ${recovered_info} ${foi} ${tmp_out_lifted} ${recovered_out_file}.nogz
+
+gzip ${recovered_out_file}.nogz -c > ${recovered_out_file}
 
 rm ${parsed_multiallelic}
 rm ${tmp_out_lifted}
 rm ${tmp_out_rejected}
 rm "${tmp_out_lifted}.tbi"
-rm ${recovered_info}`
+rm ${recovered_info}
+rm ${recovered_out_file}.nogz
