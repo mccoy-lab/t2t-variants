21977ceb61a3a650b048c9ac385ad5c6547ec14c
Dylan Taylor <54147660+dtaylo95@users.noreply.github.com>
2021-10-21 18:38:43 -0400

    Update subset_failures_to_no-mismatches.sh

--- liftover_vcfs/subset_failures_to_no-mismatches.sh
+++ liftover_vcfs/subset_failures_to_no-mismatches.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rejected_file=$1
-output_position_failed=$2
+output_position_failed=$2 # should end with .gz
 
 subset_script=subset_failures_to_no_mismatches.py
 
-python ${subset_script} ${rejected_file} > ${output_position_failed}
+python ${subset_script} ${rejected_file} | gzip -c > ${output_position_failed}
