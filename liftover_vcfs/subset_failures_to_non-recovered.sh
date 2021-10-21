cad0173af7b74db563fc27f8b6ee0ab9ea382345
Dylan Taylor <54147660+dtaylo95@users.noreply.github.com>
2021-10-21 18:39:59 -0400

    Update subset_failures_to_non-recovered.sh

--- liftover_vcfs/subset_failures_to_non-recovered.sh
+++ liftover_vcfs/subset_failures_to_non-recovered.sh
@@ -2,8 +2,11 @@
 
 recovered_file=$1
 full_rejected_file=$2
-not_recovered_output=$3
+not_recovered_output=$3 # should end with .gz
 
 subset_script=subset_failures_to_non-recovered.py
 
-python ${subset_script} ${recovered_file} ${full_rejected_file} ${not_recovered_output}
+python ${subset_script} ${recovered_file} ${full_rejected_file} ${not_recovered_output}.nogz
+
+gzip -c ${not_recovered_output}.nogz > ${not_recovered_output}
+rm ${not_recovered_output}.nogz
